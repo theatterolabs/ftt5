f60dcc77a1de6cf1d74e5bd56d970e6ade25e204
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-06-23 21:16:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1953,8 +1953,8 @@ function CGame(a, d, b) {
         d = s_oSpriteLibrary.getSprite("pole_2");
         
         pole2 = new CPole(CANVAS_WIDTH_HALF, CANVAS_HEIGHT - 193, d, F);
-        pole2.alpha = 0;
         
+        pole2.setVisible(!1);
         
 
        
@@ -1963,6 +1963,7 @@ function CGame(a, d, b) {
         A.createHelpPanel()
     };
     this.launchCountdown = function() {
+        pole2.setVisible(!1);
         var a = createBitmap(s_oSpriteLibrary.getSprite("3"));
         a.x = CANVAS_WIDTH / 2;
         a.y = CANVAS_HEIGHT / 2;
@@ -2046,8 +2047,8 @@ function CGame(a, d, b) {
             l = !0;
             playSound("crowd_ohhh", 1, !1);
             A.createAnimText(TEXT_BOWLED, 48, !1, null, 300, this.afterBallMissed);
-        pole0.alpha = 0;
-        pole2.alpha = 1;
+        pole0.setVisible(!1);
+        pole2.setVisible(!0);
             r.setVisible(!1);
             y--;
             A.refreshLivesText(y)
